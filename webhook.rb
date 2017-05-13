90a0c2a6604b187f8a3198feec83817e39a54feb
Basile Koko <basile_koko@hotmail.com>
2017-05-13 23:31:39 +0100

    adding initialize method

--- webhook.rb
+++ webhook.rb
@@ -8,5 +8,9 @@ class WebHook
   OWNER = "basilekoko"
   REPO = "ruby_koans"
 
-  
+  def initialize
+    @pull_request = Faraday.get("https://api.github.com/repos/#{OWNER}/#{REPO}/pulls?access_token=#{ENV['ACCESS_TOKEN']}")
+    @merge_close = Faraday.get("https://api.github.com/repos/#{OWNER}/#{REPO}/pulls?state=closed?access_token=#{ENV['ACCESS_TOKEN']}")
+  end
+
 end
