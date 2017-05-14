9a57266e9d7a7d5483436f52c7df83a9e542dc40
Basile Koko <basile_koko@hotmail.com>
2017-05-14 17:10:56 +0100

    making slack message method private

--- lib/webhook.rb
+++ lib/webhook.rb
@@ -12,16 +12,6 @@ class WebHook
     @event = Faraday.get("https://api.github.com/repos/#{owner}/#{repo}/events?access_token=#{ENV['ACCESS_TOKEN']}")
   end
 
-  def slack_message(webhook_url, channel, username, text)
-    payload = {
-      :channel  => channel,
-      :username => username,
-      :text     => text
-    }.to_json
-    cmd = "curl -X POST --data-urlencode 'payload=#{payload}' #{webhook_url}"
-    system(cmd)
-  end
-
   def pull_notification
     pull_request_url = JSON.parse(event.body).first['payload']['pull_request']['html_url']
     slack_message("#{ENV['SLACK_URL']}", '#general', 'pull_request_hook', "A pull request has been made, please access it here:  #{pull_request_url}")
@@ -37,6 +27,18 @@ class WebHook
     elsif event_type == "PushEvent"
     merge_notification
     end
+  end
+
+  private
+  
+  def slack_message(webhook_url, channel, username, text)
+    payload = {
+      :channel  => channel,
+      :username => username,
+      :text     => text
+    }.to_json
+    cmd = "curl -X POST --data-urlencode 'payload=#{payload}' #{webhook_url}"
+    system(cmd)
   end
 end
 
