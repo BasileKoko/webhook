b16b6cb9d326f702e4017ffa22d67b009b79d7f1
Basile Koko <basile_koko@hotmail.com>
2017-05-14 15:32:33 +0100

    adding method slack_message

--- webhook.rb
+++ webhook.rb
@@ -13,4 +13,14 @@ class WebHook
     @event = Faraday.get("https://api.github.com/repos/#{@owner}/#{@repo}/events?access_token=#{ENV['ACCESS_TOKEN']}")
   end
 
+  def slack_message(webhook_url, channel, username, text)
+    payload = {
+      :channel  => channel,
+      :username => username,
+      :text     => text
+    }.to_json
+    cmd = "curl -X POST --data-urlencode 'payload=#{payload}' #{webhook_url}"
+    system(cmd)
+  end
+
 end
